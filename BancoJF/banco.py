498688838affde3623ca1eaad2b98ba169f46af0
jfjoaofilho <joaofilho1467@gmail.com>
2022-04-28 11:40:56 -0300

    modificado banco.py em 28/04/2022

--- BancoJF/banco.py
+++ BancoJF/banco.py
@@ -1,1 +1,54 @@
+from Conta.conta import Conta
+
+
 class Banco:
+    def __init__(self):
+        self.contas = [None] * 100
+        self.indice = 0
+
+    def cadastrar(self, conta: Conta):
+        self.contas[self.indice] = conta
+        self.indice += 1
+
+    def procurar_conta(self, numero):
+        i = 0
+        achou = False
+        while achou is False and i < self.indice:
+            if self.contas[i].get_numero() == numero:
+                achou = True
+            else:
+                i += 1
+        if achou is True:
+            return self.contas[i]
+        else:
+            return None
+
+    def creditar(self, numero, valor):
+        conta = self.procurar_conta(numero)
+        if conta:
+            conta.creditar(valor)
+        else:
+            print("Conta Inexistente!")
+
+    def debitar(self, numero, valor):
+        conta = self.procurar_conta(numero)
+        if conta:
+            conta.debitar(valor)
+        else:
+            print("Conta Inexistente!")
+
+    def saldo(self, numero):
+        conta = self.procurar_conta(numero)
+        if conta:
+            conta.get_saldo()
+        else:
+            print("Conta Inexistente!")
+
+    def transferir(self, origem, destino, valor):
+        conta = self.procurar_conta(origem)
+        conta1 = self.procurar_conta(destino)
+
+        if (conta == True and conta1 == True):
+            conta.debitar(valor)
+            conta1.creditar(valor)
+
